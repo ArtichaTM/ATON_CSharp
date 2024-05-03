d748865bfa619f8872af627a9a1c7026c9454a41
Articha <tima-1324@mail.ru>
2024-05-03 15:58:07 +0300

    Initialized dotnet webapi

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,39 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ATON Api",
+        Description = "ATON Web Api",
+        // TermsOfService = new Uri("https://example.com/terms"),
+        // Contact = new OpenApiContact
+        // {
+        //     Name = "Example Contact",
+        //     Url = new Uri("https://example.com/contact")
+        // },
+        // License = new OpenApiLicense
+        // {
+        //     Name = "Example License",
+        //     Url = new Uri("https://example.com/license")
+        // }
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
